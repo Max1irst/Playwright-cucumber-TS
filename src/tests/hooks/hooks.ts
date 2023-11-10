fbec8e482b647967bcc47cbf0dde10c1194ccb7a
Maksim <maksim.bobrov@nuacom.net>
2023-11-10 12:15:57 +0200

    reports folder added

--- src/tests/hooks/hooks.ts
+++ src/tests/hooks/hooks.ts
@@ -1,17 +1,33 @@
-import { BeforeAll, AfterAll, setDefaultTimeout } from "@cucumber/cucumber";
-import { chromium, Browser, Page } from "@playwright/test";
+import { BeforeAll, AfterAll, setDefaultTimeout, Before, After, Status, AfterStep } from "@cucumber/cucumber";
+import { chromium, Browser, Page, BrowserContext } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
 let browser: Browser
-let page: Page
+let context: BrowserContext
 setDefaultTimeout(60 * 2000)
+
 BeforeAll(async function () {
     browser = await chromium.launch({ headless: false })
-    page = await browser.newPage()
+})
+
+Before(async function () {
+    context = await browser.newContext()
+    const page = await context.newPage()
     pageFixture.page = page
 })
 
-AfterAll(async function () {
+AfterStep(async function ({ pickle, result }) {
+    if (result?.status == Status.FAILED) {
+        const img = await pageFixture.page.screenshot({ path: `./reports/screenshots/${pickle}.png` })
+        this.attach(img, 'image/png')
+    }
+})
+
+After(async function () {
     await pageFixture.page.close()
+    await context.close()
+})
+
+AfterAll(async function () {
     await browser.close()
 })
